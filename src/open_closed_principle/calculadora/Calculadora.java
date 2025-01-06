4079f098e9d99834a81300eae99d0336d8f4228d
Luana Silva <luana.dossantos@zup.com.br>
2025-01-06 16:27:58 -0300

    feat: adiciona classe Calculadora e Operacao ao package open_closed_principle

---
+++ src/open_closed_principle/calculadora/Calculadora.java
@@ -0,0 +1,7 @@
+package open_closed_principle.calculadora;
+
+import open_closed_principle.operacao.Operacao;
+
+public class Calculadora implements Operacao {
+
+}
---
+++ src/open_closed_principle/operacao/Operacao.java
@@ -0,0 +1,6 @@
+package open_closed_principle.operacao;
+
+public interface Operacao {
+
+
+}
