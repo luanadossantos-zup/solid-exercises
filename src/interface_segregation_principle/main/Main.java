a992d777c26c2e0896dfb5263609bc167ad7da8c
Luana Silva <luana.dossantos@zup.com.br>
2025-01-07 15:17:55 -0300

    feat: instancia ProgramadorImpl e AtendenteImpl na Main

--- src/interface_segregation_principle/main/Main.java
+++ src/interface_segregation_principle/main/Main.java
@@ -1,4 +1,15 @@
 package interface_segregation_principle.main;
 
+import interface_segregation_principle.atendente.AtendenteImpl;
+import interface_segregation_principle.programador.ProgramadorImpl;
+
 public class Main {
+
+    public static void main(String[] args) {
+        ProgramadorImpl programador = new ProgramadorImpl();
+        AtendenteImpl atendente = new AtendenteImpl();
+
+        atendente.atenderCliente();
+        programador.programar();
+    }
 }
