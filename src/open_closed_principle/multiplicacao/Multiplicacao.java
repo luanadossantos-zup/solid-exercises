43c70bcb0330b49a123a3a8e733982b60bd6ea2e
Luana Silva <luana.dossantos@zup.com.br>
2025-01-07 10:55:33 -0300

    feat: adiciona multiplicação

--- src/open_closed_principle/multiplicacao/Multiplicacao.java
+++ src/open_closed_principle/multiplicacao/Multiplicacao.java
@@ -1,4 +1,10 @@
 package open_closed_principle.multiplicacao;
 
-public class Multiplicacao {
+import open_closed_principle.operacao.Operacao;
+
+public class Multiplicacao implements Operacao {
+    @Override
+    public int calcular(int a, int b) {
+        return a * b;
+    }
 }
