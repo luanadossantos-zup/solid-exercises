8215692642829abf733e9f3640bb2a752875d1c1
Luana Silva <luana.dossantos@zup.com.br>
2025-01-08 10:22:52 -0300

    feat: instancia teclado como dispositivo de entrada

--- src/dependency_inversion_principle/computador/Computador.java
+++ src/dependency_inversion_principle/computador/Computador.java
@@ -1,7 +1,6 @@
 package dependency_inversion_principle.computador;
 
 import dependency_inversion_principle.dispositivo_entrada.DispositivoEntrada;
-import dependency_inversion_principle.teclado.Teclado;
 
 public class Computador {
 
--- src/dependency_inversion_principle/main/Main.java
+++ src/dependency_inversion_principle/main/Main.java
@@ -1,8 +1,20 @@
 package dependency_inversion_principle.main;
 
+import dependency_inversion_principle.computador.Computador;
+import dependency_inversion_principle.dispositivo_entrada.DispositivoEntrada;
+import dependency_inversion_principle.teclado.Teclado;
+
 public class Main {
 
     public static void main(String[] args) {
+
+
+        DispositivoEntrada teclado = new Teclado();
+        Computador computadorComTeclado = new Computador(teclado);
+        computadorComTeclado.digitar();
+
+
+
 
     }
 }
