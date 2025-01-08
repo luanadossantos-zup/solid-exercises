d9a8a067dcfdb59ff9e062754685a76ed38abb8a
Luana Silva <luana.dossantos@zup.com.br>
2025-01-08 10:04:15 -0300

    feat: adiciona Main e construtor na classe Computador

--- src/dependency_inversion_principle/computador/Computador.java
+++ src/dependency_inversion_principle/computador/Computador.java
@@ -5,5 +5,13 @@ import dependency_inversion_principle.teclado.Teclado;
 
 public class Computador {
 
+    private DispositivoEntrada dispositivoEntrada;
 
+    public Computador(DispositivoEntrada dispositivoEntrada) {
+        this.dispositivoEntrada = dispositivoEntrada;
+    }
+
+    public void digitar () {
+        dispositivoEntrada.usar();
+    }
 }
---
+++ src/dependency_inversion_principle/main/Main.java
@@ -0,0 +1,8 @@
+package dependency_inversion_principle.main;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
